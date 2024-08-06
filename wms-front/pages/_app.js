3e2663d28787dc83935b7413a125827326df44a1
Soowan <ale783@naver.com>
2024-08-06 09:06:34 +0900

    fix the Header error in _app.js

--- wms-front/pages/_app.js
+++ wms-front/pages/_app.js
@@ -6,14 +6,13 @@ import HeaderLinks from "/components/Header/HomeHeaderLinks.js";
 import "/styles/scss/nextjs-material-kit.scss?v=1.2.0";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-// material-kit을 쓰기 위한 글로벌 css 선언
-
 export default class MyApp extends App {
   componentDidMount() {
     // let comment = document.createComment(``);
     // document.insertBefore(comment, document.documentElement);
   }
   static async getInitialProps({ Component, router, ctx }) {
+
     let pageProps = {};
 
     if (Component.getInitialProps) {
@@ -24,7 +23,13 @@ export default class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, router } = this.props;
+
+    // Define the routes where the header should not be displayed
+    const noHeaderRoutes = ["/user/[id]"];
+
+    // Check if the current route matches any of the routes in noHeaderRoutes
+    const shouldDisplayHeader = !noHeaderRoutes.includes(router.pathname);
 
     return (
       <React.Fragment>
@@ -36,13 +41,15 @@ export default class MyApp extends App {
           <title>ADN project Template finding</title>
         </Head>
         <SessionProvider session={pageProps.session}>
-          <Header
-            brand="FIT-BOX"
-            rightLinks={<HeaderLinks />}
-            fixed
-            color="transparent"
-            changeColorOnScroll={{ height: 400, color: "white" }}
-          />
+          {shouldDisplayHeader && (
+            <Header
+              brand="FIT-BOX"
+              rightLinks={<HeaderLinks />}
+              fixed
+              color="transparent"
+              changeColorOnScroll={{ height: 400, color: "white" }}
+            />
+          )}
           <Component {...pageProps} />
         </SessionProvider>
       </React.Fragment>
